802ffc0ce6c94296f2cfca342743fdfe43a1baf3
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2017-01-07 11:43:13 -0800

    Initial commit

---
+++ resnet.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+class ResNet:
+    def __init__(self, layer_count):
+        pass
+
+    def train_model(self, session, inputs, labels):
+        pass
+
+    def get_accuracy(self, session, inputs, labels):
+        pass
---
+++ train.py
@@ -0,0 +1,7 @@
+from resnet import ResNet
+import tensorflow as tf
+
+model = ResNet(5)
+
+with tf.Session() as session:
+    pass
